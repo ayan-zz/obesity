d4cdc135886dd86c0623d1614fac4f01bf2540de
ayan-zz <64850346+ayan-zz@users.noreply.github.com>
2023-06-04 04:12:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def predict_datapoint():
         return render_template("results.html",results=results)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=5000)
     
     
      
